21fa8ec2e3c3269a418112319b314bd6679ddf2b
monwire <monwire.dev@gmail.com>
2026-01-06 10:36:55 -0300

    finalizando estudo

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -11,8 +11,13 @@ class ProductsController {
 
   create(request: Request, response: Response) {
     const bodySchema = z.object({
-      name: z.string(),
-      price: z.number(),
+      name: z
+        .string({ required_error: "Name is required!" })
+        .trim()
+        .min(6, { message: "Name must be 6 or more characters" }),
+      price: z
+        .number({ required_error: "Price is required!" })
+        .positive({ message: "Price must be positive!" }),
     })
 
     const { name, price } = bodySchema.parse(request.body)
